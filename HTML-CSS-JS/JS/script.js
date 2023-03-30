49385fd17c01205d6d13157a9cf9c869a89b76f5
Sime-De-Moai <123648814+Sime-De-Moai@users.noreply.github.com>
2023-03-30 11:46:42 +0200

    Website publish #1
    - Added images
    - Folder system yes

---
+++ HTML-CSS-JS/JS/script.js
@@ -0,0 +1,123 @@
+window.smoothScroll = function(target) {
+    var scrollContainer = target;
+    do {
+        scrollContainer = scrollContainer.parentNode;
+        if (!scrollContainer) return;
+        scrollContainer.scrollTop += 1;
+    } while (scrollContainer.scrollTop == 0);
+
+    var targetY = 0;
+    do { 
+        if (target == scrollContainer) break;
+        targetY += target.offsetTop - 50;
+    } while (target = target.offsetParent);
+
+    scroll = function(c, a, b, i) {
+        i++; if (i > 30) return;
+        c.scrollTop = a + (b - a) / 30 * i;
+        setTimeout(function(){ scroll(c, a, b, i); }, 1);
+    }
+    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+}
+
+function copy() {
+    var copyText = document.getElementById("mail")
+    copyText.select();
+    copyText.setSelectionRange(0,99999);
+    navigator.clipboard.writeText(copyText.value);
+    alert("Copied the mail.")
+}
+
+function change_1()
+{
+document.getElementById("h_1").style.visibility="visible";
+}
+function changeB_1()
+{
+document.getElementById("h_1").style.visibility="hidden";
+}
+
+
+function change_2()
+{
+document.getElementById("h_2").style.visibility="visible";
+}
+function changeB_2()
+{
+document.getElementById("h_2").style.visibility="hidden";
+}
+
+
+function change_3()
+{
+document.getElementById("h_3").style.visibility="visible";
+}
+function changeB_3()
+{
+document.getElementById("h_3").style.visibility="hidden";
+}
+
+
+function change_4()
+{
+document.getElementById("h_4").style.visibility="visible";
+}
+function changeB_4()
+{
+document.getElementById("h_4").style.visibility="hidden";
+}
+
+
+function change_5()
+{
+document.getElementById("h_5").style.visibility="visible";
+}
+function changeB_5()
+{
+document.getElementById("h_5").style.visibility="hidden";
+}
+
+function change_6()
+{
+document.getElementById("h_6").style.visibility="visible";
+}
+function changeB_6()
+{
+document.getElementById("h_6").style.visibility="hidden";
+}
+
+function change_7()
+{
+document.getElementById("h_7").style.visibility="visible";
+}
+function changeB_7()
+{
+document.getElementById("h_7").style.visibility="hidden";
+}
+
+function change_8()
+{
+document.getElementById("h_8").style.visibility="visible";
+}
+function changeB_8()
+{
+document.getElementById("h_8").style.visibility="hidden";
+}
+
+function change_9()
+{
+document.getElementById("h_9").style.visibility="visible";
+}
+function changeB_9()
+{
+document.getElementById("h_9").style.visibility="hidden";
+}
+
+function change_10()
+{
+document.getElementById("h_10").style.visibility="visible";
+}
+function changeB_10()
+{
+document.getElementById("h_10").style.visibility="hidden";
+}
